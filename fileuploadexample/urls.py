79872a688ecca623be6d1cffebfadcf16919d528
Abbas Naqvi <snaqvi.bscs16seecs@seecs.edu.pk>
2020-06-17 19:40:30 +0500

    Done

--- fileuploadexample/urls.py
+++ fileuploadexample/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('upload/', include('uploadapp.urls')),
+    path('classification/', include('uploadapp.urls')),
 ]
 
 
